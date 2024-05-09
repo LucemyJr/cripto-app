a0ca25f94bb53aa1965b15cfa9ce3d3700d3c3d4
Lucemy Ferreira <lucemyjunior@hotmail.com>
2024-05-09 14:35:15 -0300

    add pages

---
+++ src/pages/detail/detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const detail = () => {
+  return (
+    <div>
+      <h1>Página Detalhes da Moeda</h1>
+    </div>
+  )
+}
+
+export default detail
---
+++ src/pages/notfound/notfound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notfound = () => {
+  return (
+    <div>
+      <h1>Erro 404, essa página não existe!</h1>
+    </div>
+  )
+}
+
+export default notfound
